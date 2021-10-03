bd84ec42dca0c6a1a0e23f6e314fcd733fc402ac
Herb Stahl <ghstahl@gmail.com>
2021-10-03 12:01:09 -0700

    added Add by type helpers

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.17
 
 require (
 	github.com/cheekybits/genny v1.0.0
-	github.com/fluffy-bunny/sarulabsdi v0.1.24
+	github.com/fluffy-bunny/sarulabsdi v0.1.26
 )
 
 require github.com/fatih/structtag v1.2.0 // indirect
--- pkg/interface-types.go
+++ pkg/interface-types.go
@@ -1,6 +1,8 @@
 package changeme
 
 import (
+	"reflect"
+
 	"github.com/cheekybits/genny/generic"
 	di "github.com/fluffy-bunny/sarulabsdi"
 )
@@ -10,3 +12,38 @@ type InterfaceType generic.Type
 
 // ReflectTypeInterfaceType used when your service claims to implement InterfaceType
 var ReflectTypeInterfaceType = di.GetInterfaceReflectType((*InterfaceType)(nil))
+
+// AddInterfaceTypeByObj adds a prebuilt obj
+func AddInterfaceTypeByObj(builder *di.Builder, obj interface{}) {
+	di.AddSingletonWithImplementedTypesByObj(builder, obj, ReflectTypeInterfaceType)
+}
+
+// AddSingletonInterfaceType adds a type that implements InterfaceType
+func AddSingletonInterfaceType(builder *di.Builder, implType reflect.Type) {
+	di.AddSingletonWithImplementedTypes(builder, implType, ReflectTypeInterfaceType)
+}
+
+// AddSingletonInterfaceTypeByFunc adds a type by a custom func
+func AddSingletonInterfaceTypeByFunc(builder *di.Builder, implType reflect.Type, build func(ctn di.Container) (interface{}, error)) {
+	di.AddSingletonWithImplementedTypesByFunc(builder, implType, build, ReflectTypeInterfaceType)
+}
+
+// AddTransientInterfaceType adds a type that implements InterfaceType
+func AddTransientInterfaceType(builder *di.Builder, implType reflect.Type) {
+	di.AddTransientWithImplementedTypes(builder, implType, ReflectTypeInterfaceType)
+}
+
+// AddTransientInterfaceTypeByFunc adds a type by a custom func
+func AddTransientInterfaceTypeByFunc(builder *di.Builder, implType reflect.Type, build func(ctn di.Container) (interface{}, error)) {
+	di.AddTransientWithImplementedTypesByFunc(builder, implType, build, ReflectTypeInterfaceType)
+}
+
+// AddScopedInterfaceType adds a type that implements InterfaceType
+func AddScopedInterfaceType(builder *di.Builder, implType reflect.Type) {
+	di.AddScopedWithImplementedTypes(builder, implType, ReflectTypeInterfaceType)
+}
+
+// AddScopedInterfaceTypeByFunc adds a type by a custom func
+func AddScopedInterfaceTypeByFunc(builder *di.Builder, implType reflect.Type, build func(ctn di.Container) (interface{}, error)) {
+	di.AddScopedWithImplementedTypesByFunc(builder, implType, build, ReflectTypeInterfaceType)
+}
